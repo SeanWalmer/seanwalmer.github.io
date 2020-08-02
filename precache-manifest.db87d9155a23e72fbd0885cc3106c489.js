c9379ce8384892024a04ca3f6be4157d42188179
SeanW <sean.walmer@outlook.com>
2020-08-01 21:11:59 -0400

    Updates

---
+++ precache-manifest.db87d9155a23e72fbd0885cc3106c489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08ccb117c678bff91d2d0dcc0bf1620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b78b4cd1fb451d24e57f",
+    "url": "/static/css/main.38f3043c.chunk.css"
+  },
+  {
+    "revision": "b20af2ae70d15c5ad6e2",
+    "url": "/static/js/2.62ce2e6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62ce2e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78b4cd1fb451d24e57f",
+    "url": "/static/js/main.e9844792.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
